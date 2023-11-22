ffc876eb0c8425a8e9f5ca9b456643d089189805
angga0310 <90076969+angga0310@users.noreply.github.com>
2023-11-22 21:36:57 +0700

    desain splash screen, login, list item, dashboard, Bottom Nav

--- app/src/main/java/com/example/reporthing/SiswaActivity.java
+++ app/src/main/java/com/example/reporthing/SiswaActivity.java
@@ -1,20 +1,28 @@
 package com.example.reporthing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 import Fragment.DasboardFragment;
+import Fragment.JadwalFragment;
 
 public class SiswaActivity extends AppCompatActivity {
 
     private long backPress;
     private Toast backToast;
 
+    BottomNavigationView bottomNavigationItemView;
 
-    DasboardFragment dasboardFragment;
     @Override
     public void onBackPressed() {
         if (backPress + 2000 > System.currentTimeMillis()){
@@ -32,9 +40,26 @@ public class SiswaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siswa);
 
-        dasboardFragment = new DasboardFragment();
+        bottomNavigationItemView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationItemView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.btn_home){
+                    getFragment(new DasboardFragment());
+                    return true;
+                }else if (item.getItemId() == R.id.btn_jadwal){
+                    getFragment(new JadwalFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+        getFragment(new DasboardFragment());
+    }
+
+    private void getFragment(Fragment fragment){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.parent_frame, dasboardFragment);
+        transaction.replace(R.id.parent_frame, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
